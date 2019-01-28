78b56b1e2428869c5129aba8b7a9bb938c89d008
Casper Smet <casper.smet@student.hu.nl>
2019-01-28 15:05:22 +0100

    Callable from game_example + one_upper operational

--- sql.py
+++ sql.py
@@ -1,51 +1,40 @@
 import mysql.connector
 
 
-
 def connect_database():
 	database = mysql.connector.connect(
-		# host='145.89.166.209',
-		host='localhost',
+		host='145.89.166.209',
+		#host='localhost',
 		user='tom',
 		password='broer'
 	)
 	return database
 
-def get_PoI():
+
+def get_PoI(database):
+	database = database
 	try:
-		database = connect_database()
 		cursor = database.cursor()
 
 		cursor.execute('use tomsbroer;')
 		cursor.execute('select * from points_of_interest order by searched desc;')
 
-		data = []
+		data = dict()
 
 		for x in cursor:
-			temp = dict()
-
-			temp['ID'] = x[0]
-			temp['Name'] = x[1]
-			temp['coordinates'] = x[2]
-			temp['searched'] = x[3]
-
-			data.append(temp)
+			data[x[1]] = eval(x[2])
+		print(data)
 	except:
 		get_PoI()
+	return data
 
-	print(data[:3])
-	return data[:3]
 
-def one_upper():
-	poi_id = 1
-	update = 'update points_of_interest set searched = searched + 1 where ID =' + str(poi_id)
-	
+def one_upper(q):
+	update = "update points_of_interest set searched = searched + 1 where name = '{}' ".format(q)
+	print(update)
 	database = connect_database()
 	cursor = database.cursor()
 	
 	cursor.execute('use tomsbroer;')
 	cursor.execute(update)
 	database.commit()
-
-# get_PoI()
-one_upper()
