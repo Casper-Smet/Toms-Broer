74e42c3196d0563400a5151437073acf251e59d6
Casper Smet <casper.smet@student.hu.nl>
2019-01-28 13:29:11 +0100

    New test version.

--- sql.py
+++ sql.py
@@ -1,26 +1,50 @@
 import mysql.connector
 
-database = mysql.connector.connect(
-	host='145.89.166.209',
-	user='tom',
-	password='broer'
-)
 
-cursor = database.cursor()
 
-cursor.execute('use tomsbroer;')
-cursor.execute('select * from points_of_interest order by searched desc;')
+def connect_database():
+	database = mysql.connector.connect(
+		host='145.89.166.209',
+		user='tom',
+		password='broer'
+	)
+	return database
 
-data = []
+def get_PoI():
+	try:
+		database = connect_database()
+		cursor = database.cursor()
 
-for x in cursor:
-	temp = dict()
+		cursor.execute('use tomsbroer;')
+		cursor.execute('select * from points_of_interest order by searched desc;')
 
-	temp['ID'] = x[0]
-	temp['Name'] = x[1]
-	temp['coordinates'] = x[2]
-	temp['searched'] = x[3]
+		data = []
 
-	data.append(temp)
+		for x in cursor:
+			temp = dict()
 
-print(data[:3])
+			temp['ID'] = x[0]
+			temp['Name'] = x[1]
+			temp['coordinates'] = x[2]
+			temp['searched'] = x[3]
+
+			data.append(temp)
+	except:
+		get_PoI()
+
+	print(data[:3])
+	return data[:3]
+
+def one_upper():
+	poi_id = 1
+	update = "UPDATE points_of_interest SET searched = searched + 1 WHERE ID =" + str(poi_id)+ ";"
+	try:
+		database = connect_database()
+		cursor = database.cursor()
+
+		cursor.execute('use tomsbroer;')
+		cursor.execute(update)
+	except:
+		one_upper()
+#get_PoI()
+one_upper()
